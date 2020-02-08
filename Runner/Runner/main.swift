3338d9a0e344260959def1e419b900579f23f037
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2020-02-08 14:13:45 +0200

    Add Runner and solver

---
+++ Runner/Runner/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Runner
+//
+//  Created by Alexey Demedetskii on 2/8/20.
+//  Copyright © 2020 Genesis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Solver/Solver/Solver.h
@@ -0,0 +1,19 @@
+//
+//  Solver.h
+//  Solver
+//
+//  Created by Alexey Demedetskii on 2/8/20.
+//  Copyright © 2020 Genesis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Solver.
+FOUNDATION_EXPORT double SolverVersionNumber;
+
+//! Project version string for Solver.
+FOUNDATION_EXPORT const unsigned char SolverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Solver/PublicHeader.h>
+
+
